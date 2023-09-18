b1dae3525a47a075f36561ae752f4a619926327c
João Matheus Lamão <joaomatheuslamao9@gmail.com>
2023-09-18 19:57:59 -0300

    Continuação dos testes

--- Engenharia SW/SecretariaInstitucional/src/testes/SecretariaTest.java
+++ Engenharia SW/SecretariaInstitucional/src/testes/SecretariaTest.java
@@ -1,5 +1,7 @@
 package testes;
 
+import entities.Aluno;
+import entities.Disciplina;
 import entities.Professor;
 import entities.Secretaria;
 import org.junit.jupiter.api.Test;
@@ -13,11 +15,34 @@ class SecretariaTest {
         Secretaria sec = new Secretaria();
         sec.cadastrarProfessor(new Professor(1, "Giuliano Bertoti"));
 
+        //teste inserção de professores
         assertEquals(1, sec.getProfessores().size());
 
         sec.cadastrarProfessor(new Professor(2, "HAHAHA"));
 
+        //teste inserção de professores
         assertEquals(2, sec.getProfessores().size());
+
+        Aluno aluno1 = new Aluno(777888, "João Matheus Lamão", true);
+
+        sec.matricularAluno(aluno1);
+        sec.getAlunos().get(0).matricularAlunoDisciplina(new Disciplina("Engenharia de Software", sec.getProfessores().get(0)));
+
+        //teste inserção de alunos (verificando o nome)
+        assertEquals(sec.getAlunos().get(0).getNome(),"João Matheus Lamão");
+
+        // teste metodo consultar matricula
+        Aluno consultaMatricula = sec.consultarMatricula(777888);
+
+        assertEquals(aluno1, consultaMatricula);
+
+
+        Aluno aluno2 = new Aluno(123456, "Ana Raquel Yamamoto Sasaki Machado", false);
+        sec.matricularAluno(aluno2);
+
+        //teste validar matricula ativa
+        assertEquals(false, sec.consultarMatricula(123456).isStatus());
+
     }
 
 }
